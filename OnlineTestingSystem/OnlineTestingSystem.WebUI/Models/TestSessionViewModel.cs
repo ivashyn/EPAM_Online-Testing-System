92317cb75a1be00eb99ed2097e81e309117e6031
ivashyn <ivashyn.vadym@gmail.com>
2017-05-11 14:05:51 +0300

    added views

---
+++ OnlineTestingSystem/OnlineTestingSystem.WebUI/Models/TestSessionViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OnlineTestingSystem.WebUI.Models
+{
+    public class TestSessionViewModel
+    {
+        public int Id { get; set; }
+        public bool IsPassed { get; set; }
+        public int Score { get; set; }
+        public DateTime TimeStart { get; set; }
+        public DateTime TimeFinish { get; set; }
+        public TimeSpan TestTime { get; set; }
+        public int UserId { get; set; }
+        public int TestId { get; set; }
+        public string TestName { get; set; }
+    }
+}
---
+++ OnlineTestingSystem/OnlineTestingSystem.WebUI/Models/TestViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OnlineTestingSystem.WebUI.Models
+{
+    public class TestViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int ScoreToPass { get; set; }
+        public byte Timelimit { get; set; }
+
+        public int QuestionCategoryId { get; set; }
+        public int NumberOfQuestions { get; set; }
+    }
+}
