3ba0457d70bb70530eaaf9f24eea95ed82598ad3
ivashyn <ivashyn.vadym@gmail.com>
2017-05-11 21:03:38 +0300

    added UserViewModel.cs and Cabinet.html :)

---
+++ OnlineTestingSystem/OnlineTestingSystem.WebUI/Models/UserViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OnlineTestingSystem.WebUI.Models
+{
+    public class UserViewModel
+    {
+        public int UserID { get; set; }
+        
+        public string FirstName { get; set; }
+        
+        public string LastName { get; set; }
+        
+        public string Login { get; set; }
+        
+        public string Password { get; set; }
+
+        public string Email { get; set; }
+
+        public string Role { get; set; }
+    }
+}
